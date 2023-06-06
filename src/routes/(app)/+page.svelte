46702a492b70af6253d2b4fa972f7b3049458e3a
rzh90 <52875611+rzh90@users.noreply.github.com>
2023-06-06 11:30:13 -0400

    update hero

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,7 +1,5 @@
-<script>
-    
-</script>
-
-<section>
-Hello
+<section class="bg-ssdarkgreen text-white">
+    <div class="mx-auto max-w-screen-xl"> <!-- wrapper -->
+        <h2>Track shipments</h2>
+    </div>
 </section>
