ad00d0f4c5caa529809959c5b5369c2d14224030
ruinaz90 <rzhao9002@gmail.com>
2023-08-06 20:57:26 -0400

    Added number of shipments this week warning

--- src/routes/(app)/account/+page.svelte
+++ src/routes/(app)/account/+page.svelte
@@ -3,10 +3,14 @@
     Also from https://supabase.com/docs/guides/auth/auth-helpers/sveltekit#server-side-data-fetching-with-rls
 -->
 <script>
+    import { ProgressRadial } from "@skeletonlabs/skeleton"
+
     export let data
     $: ({ testTable, user, supabase, session } = data)
 
     let selectedOrders = []
+    const today = new Date()
+    let shipmentsThisWeek = 0
 
     function toggleSelected(e) {
         let ids = []
@@ -34,6 +38,12 @@
         testTable = getOrders()
         selectedOrders = []
     }
+
+    for(let element of data.testTable) {
+        let daysLeft = Math.floor((Date.parse(element.ship_date) - today) / (24 * 3600 * 1000))
+        if(daysLeft >= 0 && daysLeft <= 7)
+            shipmentsThisWeek++
+    }
 </script>
 
 <p>Hi {user.email}</p>
@@ -50,51 +60,66 @@
     {/if}
 </div>
 {#await testTable}
-    <p>Loading</p>
+    <ProgressRadial />
 {:then testTable}
-{#if testTable.length}
-<div class="table-container mt-6">
-    <table class="table table-hover">
-        <thead>
-            <tr>
-                <th><input class="checkbox" type="checkbox" on:change={toggleSelected}></th>
-                <th>PO</th>
-                <th>Customer PO</th>
-                <th>Retailer</th>
-                <th>Ship Date</th>
-                <th>ETD</th>
-                <th>ETA</th>
-                <th>Ship Docs</th>
-                <th>Received</th>
-                <th>Invoiced</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            {#each testTable as order}
-                <tr>
-                    <td>
-                        <form>
-                        <input class="checkbox" type="checkbox" name="selectedOrders" id="selectedOrders" value={order.id} bind:group={selectedOrders}>
-                        </form>
-                    </td>
-                    <td>{order.po}</td>
-                    <td>{order.customerpo}</td>
-                    <td>{order.retailer}</td>
-                    <td>{#if order.ship_date} {order.ship_date} {/if}</td>
-                    <td>{#if order.etd} {order.etd} {/if}</td>
-                    <td>{#if order.eta} {order.eta} {/if}</td>
-                    <td>{#if order.ship_docs} Received {:else} Not received {/if}</td>
-                    <td>{#if order.received} Yes {:else} No {/if}</td>
-                    <td>{#if order.invoiced} Yes {:else} No {/if}</td>
-                    <td><a href={`/account/edit/${order.id}`} class="anchor">Edit</a></td>
-                </tr>
-            {/each}
-        </tbody>
-    </table>
-</div>
+    {#if testTable.length}
+        {#if shipmentsThisWeek > 0}
+            <aside class="alert variant-filled-warning">
+                <div class="alert-message">
+                    <p>
+                        {shipmentsThisWeek}
+                        {#if shipmentsThisWeek > 1}
+                            shipments this week.
+                        {:else}
+                            shipment this week.
+                        {/if}
+                    Make sure the ETD/ETA is confirmed!</p>
+                </div>
+            </aside>
+        {/if}
 
-{:else}
-<p>No orders yet</p>
-{/if}
+        <div class="table-container mt-6">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th><input class="checkbox" type="checkbox" on:change={toggleSelected}></th>
+                        <th>PO</th>
+                        <th>Customer PO</th>
+                        <th>Retailer</th>
+                        <th>Ship Date</th>
+                        <th>ETD</th>
+                        <th>ETA</th>
+                        <th>Ship Docs</th>
+                        <th>Received</th>
+                        <th>Invoiced</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {#each testTable as order}
+                        <tr>
+                            <td>
+                                <form>
+                                <input class="checkbox" type="checkbox" name="selectedOrders" id="selectedOrders" value={order.id} bind:group={selectedOrders}>
+                                </form>
+                            </td>
+                            <td>{order.po}</td>
+                            <td>{order.customerpo}</td>
+                            <td>{order.retailer}</td>
+                            <td>{#if order.ship_date} {order.ship_date} {/if}</td>
+                            <td>{#if order.etd} {order.etd} {/if}</td>
+                            <td>{#if order.eta} {order.eta} {/if}</td>
+                            <td>{#if order.ship_docs} Received {:else} Not received {/if}</td>
+                            <td>{#if order.received} Yes {:else} No {/if}</td>
+                            <td>{#if order.invoiced} Yes {:else} No {/if}</td>
+                            <td><a href={`/account/edit/${order.id}`} class="anchor">Edit</a></td>
+                        </tr>
+                    {/each}
+                </tbody>
+            </table>
+        </div>
+
+    {:else}
+        <p>No orders yet</p>
+    {/if}
 {/await}
